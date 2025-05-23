585c9ba2da93acd524528ef1155fc9004dd17255
Nicolas Peugnet <nicolas.peugnet@lip6.fr>
2025-05-23 11:44:37 +0200

    Initial dmarc-reject version (hard coded)

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/club-1/dmarc-reject
+
+go 1.22.0
+
+require (
+	github.com/emersion/go-milter v0.4.1
+	github.com/emersion/go-msgauth v0.7.0
+)
+
+require github.com/emersion/go-message v0.18.1 // indirect
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"slices"
+	"strings"
+	"syscall"
+
+	"github.com/emersion/go-milter"
+	"github.com/emersion/go-msgauth/authres"
+)
+
+var (
+	// TODO: make these variables configurable, maybe based on a TOML config file?
+	identity      string   = "mail.club1.fr"
+	listenURI     string   = "unix:///var/spool/postfix/dmarc-reject/dmarc-reject.sock"
+	rejectDomains []string = []string{"gmail.com"}
+)
+
+type Session struct {
+	milter.NoOpMilter
+}
+
+func shouldRejectDMARCRes(result *authres.DMARCResult) bool {
+	if result.Value == authres.ResultPass {
+		return false
+	}
+	if !slices.Contains(rejectDomains, result.From) {
+		return false
+	}
+	return true
+}
+
+func (s *Session) Header(name string, value string, m *milter.Modifier) (milter.Response, error) {
+	if !strings.EqualFold(name, "Authentication-Results") {
+		return milter.RespContinue, nil
+	}
+	id, results, err := authres.Parse(value)
+	if err != nil {
+		// Reject in case we can't parse, an AR header, because we cannot
+		// handle it better than that.
+		return milter.RespReject, err
+	}
+
+	if !strings.EqualFold(id, identity) {
+		// Not our Authentication-Results, ignore the field
+		return milter.RespContinue, nil
+	}
+
+	for _, result := range results {
+		if r, ok := result.(*authres.DMARCResult); ok {
+			if shouldRejectDMARCRes(r) {
+				return milter.RespReject, nil
+			}
+		}
+	}
+
+	return milter.RespContinue, nil
+}
+
+func main() {
+	if identity == "" {
+		var err error
+		identity, err = os.Hostname()
+		if err != nil {
+			log.Fatal("Failed to read hostname: ", err)
+		}
+	}
+
+	parts := strings.SplitN(listenURI, "://", 2)
+	if len(parts) != 2 {
+		log.Fatal("Invalid listen URI")
+	}
+	listenNetwork, listenAddr := parts[0], parts[1]
+
+	s := milter.Server{
+		NewMilter: func() milter.Milter {
+			return &Session{}
+		},
+		Actions:  milter.OptAddHeader | milter.OptChangeHeader,
+		Protocol: milter.OptNoConnect | milter.OptNoHelo | milter.OptNoBody | milter.OptNoMailFrom | milter.OptNoRcptTo,
+	}
+
+	ln, err := net.Listen(listenNetwork, listenAddr)
+	if err != nil {
+		log.Fatal("Failed to setup listener: ", err)
+	}
+
+	// Closing the listener will unlink the unix socket, if any
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		if err := s.Close(); err != nil {
+			log.Fatal("Failed to close server: ", err)
+		}
+	}()
+
+	log.Println("Milter listening at", listenURI)
+	if err := s.Serve(ln); err != nil && err != milter.ErrServerClosed {
+		log.Fatal("Failed to serve: ", err)
+	}
+}
